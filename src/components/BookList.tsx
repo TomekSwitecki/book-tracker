4e53ab3c83265fa68097e1f5520f11bc781eaeb2
Tomek Switecki <ttwitecki@gmail.com>
2026-06-13 19:52:14 +0200

    type array fix

--- src/components/BookList.tsx
+++ src/components/BookList.tsx
@@ -1,7 +1,7 @@
 import type { Book } from "../types/books";
 import "../styles/bookItem.scss";
 import BookCard from "./BookCard";
-function BookList({ books, onBookDelete }: { books: Book; onBookDelete: (id: number) => void }) {
+function BookList({ books, onBookDelete }: { books: Book[]; onBookDelete: (id: number) => void }) {
     return (
         <div className="book-item__wrapper">
             <h2>Books</h2>
